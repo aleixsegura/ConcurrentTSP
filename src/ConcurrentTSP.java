09793f2879dbb4e229a070c8665cfa328a14160c
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2024-04-29 23:41:04 +0200

    removed: authors ID data

--- src/ConcurrentTSP.java
+++ src/ConcurrentTSP.java
@@ -2,8 +2,8 @@
 Práctica 2.
 Código fuente: ConcurrentTSP.java
 Grau Informàtica
-48056540H - Aleix Segura Paz.
-21161168H - Aniol Serrano Ortega.
+Aleix Segura Paz.
+Aniol Serrano Ortega.
 --------------------------------------------------------------- */
 import jdk.jshell.spi.ExecutionControl;
 

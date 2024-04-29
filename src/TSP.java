01441d0e29d92f3273a028946bbdd4cd925110a5
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2024-04-29 23:41:39 +0200

    removed: authors ID data

--- src/TSP.java
+++ src/TSP.java
@@ -2,8 +2,8 @@
 Práctica 2.
 Código fuente: TSP.java
 Grau Informàtica
-48056540H - Aleix Segura Paz.
-21161168H - Aniol Serrano Ortega.
+Aleix Segura Paz.
+Aniol Serrano Ortega.
 --------------------------------------------------------------- */
 
 import java.io.File;
